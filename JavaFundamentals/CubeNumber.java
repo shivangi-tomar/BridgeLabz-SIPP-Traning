6fa9e31d246941e518340fb896cdd57c1adaf335
Shivangi Tomar <shivangi.tomar2004@gmail.com>
2025-06-23 13:08:48 +0530

    Cleaned OOPs branch by deleting all previous files

--- JavaFundamentals/CubeNumber.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class CubeNumber {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = input.nextInt();
-        System.out.println("Cube of the number is " + (num * num * num));
-    }
-}
